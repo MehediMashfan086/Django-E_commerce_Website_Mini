ac185c2d89b4bbeb789df51d21b3ea9e6259aa42
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-22 23:07:44 +0600

    Saving to GitHub

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('store', views.store, name = 'store'),
-    path('cart', views.cart, name = 'cart'),
-    path('checkout', views.checkout, name = 'checkout')
+    path('store/', views.store, name = 'store'),
+    path('cart/', views.cart, name = 'cart'),
+    path('checkout/', views.checkout, name = 'checkout')
 ]
