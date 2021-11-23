ef01eef70620cd7fac9597d267d0e347f14f23f1
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-23 11:00:58 +0600

    Saving to GitHub

--- store/models.py
+++ store/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.ImageField()
+    
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    cart_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    completed = models.BooleanField(default=False)
+    
+class Cartitems(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product =  models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
