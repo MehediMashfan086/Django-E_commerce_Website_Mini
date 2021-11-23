30e4e054eec70485077a9eb3028121d2a1f42259
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-23 12:23:20 +0600

    Saving to GitHub

--- store/models.py
+++ store/models.py
@@ -7,20 +7,33 @@ class Customer(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     
+    def __str__(self):
+        return self.name
+    
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     image = models.ImageField()
+    
+    def __str__(self):
+        return self.name
     
 class Cart(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     cart_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     completed = models.BooleanField(default=False)
     
+    def __str__(self):
+        return str(self.id)
+    
+    
 class Cartitems(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product =  models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.product.name
     
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
@@ -29,3 +42,6 @@ class ShippingAddress(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.address
--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('', views.store, name = 'store'),
     path('store/', views.store, name = 'store'),
     path('cart/', views.cart, name = 'cart'),
     path('checkout/', views.checkout, name = 'checkout'),
